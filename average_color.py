b932f66bacdd08c77769e46769cf2bdd399fbc2d
Ole August <oleastole@gmail.com>
2022-03-03 13:23:49 +0100

    feat: add hex-value

--- average_color.py
+++ average_color.py
@@ -9,8 +9,12 @@ def average_color(image_path):
         b_avg = int(np.average(b))
         g_avg = int(np.average(g))
         r_avg = int(np.average(r))
-        average_color = (r_avg, g_avg, b_avg)
-        return average_color
+
+        average_color_rgb = (r_avg, g_avg, b_avg)
+        average_color_hex = '#{:02x}{:02x}{:02x}'.format(r_avg, g_avg, b_avg)
+        color_link = f'https://www.color-hex.com/color/{average_color_hex[1:]}'
+
+        return [average_color_rgb, average_color_hex, color_link]
     except:
         print("Error: Cannot read image from given path")
         return None
@@ -19,8 +23,10 @@ def main():
     if (len(sys.argv[1:]) < 1): return
 
     res = average_color(sys.argv[1])
-    if (res is not None):
-        print(res)
+    if (res is None): return
+    
+    for color in res:
+        print(color)
 
 if __name__ == '__main__':
     main()
